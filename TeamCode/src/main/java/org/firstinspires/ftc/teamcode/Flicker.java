9ffc57d6afb2be4c91ea24821acc6c94393882e7
Sid K <sidkhillon24@gmail.com>
2021-03-13 16:51:54 -0800

    Update Flicker.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Flicker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Flicker.java
@@ -9,7 +9,9 @@ public class Flicker {
     final float initialAngle = 0;
     final float extendedAngle = 60;
 
-    final float delay = 20;
+    final float delay = 300;
+
+    boolean firstClick = true;
 
     public Flicker(Component servo){
         this.flickerServo = (StepperServo) servo;
@@ -17,15 +19,18 @@ public class Flicker {
     }
 
     //Run when button pressed
-    public void flick(boolean clicked){
-        if (clicked){
-            flickerServo.setAngle(extendedAngle);
-            timeOfClick = System.currentTimeMillis();
-        } else {
-            if (System.currentTimeMillis() - timeOfClick >= delay){
-                flickerServo.setAngle(initialAngle);
+    public void flickControl(boolean clicked){
+        if (clicked) {
+            if (firstClick && System.currentTimeMillis() - timeOfClick >= delay){
+                flickerServo.setAngle(extendedAngle);
+                timeOfClick = System.currentTimeMillis();
+                firstClick = false;
             }
         }
-    }
 
+        if (System.currentTimeMillis() - timeOfClick >= (delay/2)){
+            flickerServo.setAngle(initialAngle);
+            firstClick = true;
+        }
+    }
 }
